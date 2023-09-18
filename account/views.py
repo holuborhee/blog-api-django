0604178184e9580da73eb889d977d06cc2f92354
olubori <daveholuborhee@gmail.com>
2023-09-18 12:25:32 +0100

    fix plain password stored in DB

--- account/views.py
+++ account/views.py
@@ -1,24 +1,14 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
+from rest_framework import mixins, generics
 
 
-class RegisterView(APIView):
+class RegisterView(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
 
-    def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
-
-        if not serializer.is_valid():
-            return Response({
-                'data': serializer.errors,
-                'message': 'Something went wrong'
-            }, status = status.HTTP_400_BAD_REQUEST)
-
-        serializer.save()
-
-        return Response({
-            'data': {},
-            'message': 'Your account is created'
-        }, status = status.HTTP_201_CREATED)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
