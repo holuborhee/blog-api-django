f73042ea5c70a388f532d2a141b17a80bd005522
olubori <daveholuborhee@gmail.com>
2023-09-20 10:13:39 +0100

    update allowed host

--- blog/settings.py
+++ blog/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog-api-django-hea1.onrender.com', '127.0.0.1']
 
 CORS_ALLOWED_ORIGINS=[
     'http://localhost:3000'
